ce0a459f530cbdf925522b4eb08e43aaafbfd9c5
paramjeet singh <paramjeet@scaleus.in>
2025-09-29 21:57:19 +0530

    add empty string logic and test cases

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
   def add(numbers)
-   
+   return 0 if numbers.strip.empty?
   end
 end
+
+calculator = StringCalculator.new
+puts calculator.add("") 
---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require_relative "../app/string_calculator"
+
+RSpec.describe StringCalculator do
+  describe "#add" do
+    it "returns 0 for an empty string" do
+      calc = StringCalculator.new
+      expect(calc.add("")).to eq(0)
+    end
+  end
+end
