4f1885c06d290327332376d56c96067d45e77037
paramjeet singh <paramjeet@scaleus.in>
2025-09-29 22:04:49 +0530

    add single number string logic and test cases

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def add(numbers)
-   return 0 if numbers.strip.empty?
+    return 0 if numbers.strip.empty?
+
+    nums = numbers.split(",").map(&:to_i)
+    return nums.first if nums.length == 1
   end
 end
 
